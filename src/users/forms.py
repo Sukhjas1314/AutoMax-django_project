794beafb20d37a8df1682ff6e1ad44c8e85e45eb
Sukhmanpreet Singh <ssbakshi2050@gmail.com>
2025-07-01 17:45:36 +0530

    Making and updating lists

---
+++ src/users/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+from .models import Location
+
+class LocationForm(forms.ModelForm):
+
+    address_1 = forms.CharField(required=True)
+    zip_code = forms.CharField(required=True)
+    class Meta:
+        model = Location
+        fields = {'address_1','address_2','city','state','zip_code'}
