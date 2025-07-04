4be0b4616beb0efbacea91e5dbbbec95a9fac045
Sukhmanpreet Singh <ssbakshi2050@gmail.com>
2025-07-04 16:26:03 +0530

    Updated Profile form

--- src/users/forms.py
+++ src/users/forms.py
@@ -1,6 +1,18 @@
 from django import forms
+from dataclasses import field
+from django.contrib.auth.models import User
+from .models import Location,Profile
 
-from .models import Location
+class UserForm(forms.ModelForm):
+    username = forms.CharField(disabled=True)
+    class Meta:
+        model = User
+        fields = ('username','first_name','last_name')
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('photo','bio','phone_number')
 
 class LocationForm(forms.ModelForm):
 
--- src/users/views.py
+++ src/users/views.py
@@ -3,7 +3,11 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.views import View
+from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
+
+from main.models import Listing
+from .forms import UserForm, ProfileForm, LocationForm
 
 # Create your views here.
 def login_view(request):
@@ -46,3 +50,29 @@ class RegisterView(View):
         else:
             messages.error(request,f'An error occured trying to register.')
             return render(request,'views/register.html',{'register_form': register_form})
+
+@method_decorator(login_required,name='dispatch')
+class ProfileView(View):
+
+    def get(self,request):
+        user_listings = Listing.objects.filter(seller=request.user.profile)
+        user_form = UserForm(instance=request.user)
+        profile_form = ProfileForm(instance=request.user.profile)
+        location_form = LocationForm(instance=request.user.profile.location)
+        return render(request,'views/profile.html',{'user_form': user_form,'profile_form':profile_form,'location_form': location_form,})
+
+
+    def post(self,request):
+        user_listings = Listing.objects.filter(seller=request.user.profile)
+        user_form = UserForm(request.POST,instance=request.user)
+        profile_form = ProfileForm(request.POST,request.FILES,instance=request.user.profile)
+        location_form = LocationForm(request.POST,instance=request.user.profile.location)
+        if user_form.is_valid() and profile_form.is_valid() and location_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            location_form.save()
+            messages.success(request,'Profile updated Successfully!')
+        else:
+            messages.error(request,'Error Updating Profile!')
+        return render(request,'views/profile.html',{'user_form': user_form,'profile_form':profile_form,'location_form': location_form,'user_listings': user_listings,})
+        
