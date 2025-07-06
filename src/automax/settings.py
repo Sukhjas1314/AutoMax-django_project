c962791ca9ad671d81e36731900568c1e7a3971a
Sukhmanpreet Singh <ssbakshi2050@gmail.com>
2025-07-06 10:46:41 +0530

    Final updates

--- src/automax/settings.py
+++ src/automax/settings.py
@@ -11,9 +11,15 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
 import os
+from distutils.util import strtobool
 
 from django.contrib.messages import constants as messages
+
+#  Initialize Environ
+env = environ.Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,10 +29,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-+3!+#0b(s&0p6kikw3v1x404zqk$@y%rrg4oa#3lwa0b9*7u$f"
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = strtobool(env('DEBUG'))
 
 ALLOWED_HOSTS = ['*']
 
@@ -81,12 +87,21 @@ WSGI_APPLICATION = "automax.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# if strtobool(env('USEDEBUGDB')):
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+# else:
+#     DATABASES = {
+#         "default": {
+#             "ENGINE": "django.db.backends.postgresql_psycopg2",
+#             "NAME": env('DBNAME'),
+            # "PASSWORD": env('DBPASSWORD')
+#         }
+#     }
 
 
 # Password validation
@@ -139,6 +154,14 @@ MEDIA_URL = '/media/'
 
 # Django Crispy Form Settings
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+# Email settings
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.mailgun.org'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
